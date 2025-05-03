c3ede04a184a7b3ef40b61b1c35353dcda5473f1
David <daviddavis@users.noreply.github.com>
2025-05-03 09:49:38 -0400

    Switch to zsh

--- home.nix
+++ home.nix
@@ -15,15 +15,27 @@
     fzf
   ];
 
-  programs.bash = {
+  programs.zsh = {
     enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    history.size = 10000;
+
+    initExtra = ''
+      # Set prompt
+      PS1='%F{cyan}%n@%m%f %F{yellow}%~%f %# '
+
+      # Basic key bindings
+      bindkey -e
+    '';
+
     shellAliases = {
       ll = "ls -la";
       g = "git";
       gaa = "git rev-parse --show-toplevel | xargs git add -A";
       gs = "git status";
       gd = "git diff";
-      wat = "echo 'wut'";
+      gc = "git commit";
     };
   };
 
